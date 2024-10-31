c201a67974e8e090a756104e88cbfd861b7db240
Brandon Collins <144741304+Brandon-Collins7@users.noreply.github.com>
2024-10-30 19:20:34 -0500

    Update name of Project4.cpp

--- Project4/Project4.cpp
+++ Project4/Project4.cpp
@@ -1,11 +1,3 @@
-/* AUTHOR: Brandon Collins
-   USER ID: blc0063
-   FILE NAME: project4_Collins_blc0063.cpp
-   VERSION: 3/27/2024
-   // compile using g++ project4_Collins_blc0063.cpp
-   // run ./a.out
- */
-
 #include <iostream>
 #include <assert.h>
 //#include <string>
