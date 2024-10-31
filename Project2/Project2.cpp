304bc1fb12b062de54f08de8278967f0b18b3b42
Brandon Collins <144741304+Brandon-Collins7@users.noreply.github.com>
2024-10-30 19:16:13 -0500

    Update and rename project2_Collins_blc0063.cpp to Project2.cpp

--- Project2/Project2.cpp
+++ Project2/Project2.cpp
@@ -1,11 +1,3 @@
-/* AUTHOR: Brandon Collins
-   USER ID: blc0063
-   FILE NAME: project2_Collins_blc0063.cpp
-   VERSION: 2/16/2024
-   // compile using g++ project2_Collins_blc0063.cpp
-   // run ./a.out
- */
-
 # include <iostream>
 # include <stdlib.h>
 # include <assert.h>
